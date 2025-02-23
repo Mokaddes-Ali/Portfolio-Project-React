723e979e560bf35029a7bcef9d064f6fdfd4a526
mokadde <mokaddes.ru2000@gmail.com>
2025-02-23 16:51:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='text-4xl text-red-600'>Welcome to My Portfolio Website App</div>
+    </>
+  )
+}
+
+export default App
